bdc7ca7d3654893c53c9e30d5d195a0681ad6ca7
SahilRout <sahilrout07@gmail.com>
2021-11-04 23:24:40 +0530

    DAY-15

--- DAY 15/DAY15-Q2.cpp
+++ DAY 15/DAY15-Q2.cpp
@@ -1,1 +1,24 @@
 //69. Sqrt(x)
+class Solution
+{
+public:
+    int mySqrt(int x)
+    {
+        long long start = 0, end = x, ans = 0;
+        while (start <= end)
+        {
+            long long mid = start + (end - start) / 2;
+            if (mid * mid <= x)
+            {
+                ans = mid;
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
