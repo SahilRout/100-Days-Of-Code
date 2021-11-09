23af898960babfa97d5835a07ead00e4f171cc29
SahilRout <sahilrout07@gmail.com>
2021-11-09 10:55:26 +0530

    DAY-10-Q1

---
+++ DAY 20/Q1.cpp
@@ -0,0 +1,67 @@
+//37. Sudoku Solver
+class Solution
+{
+public:
+    bool isSafe(vector<vector<char>> &board, int row, int col, char num)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (board[row][i] == num)
+                return false;
+        }
+        for (int j = 0; j < 9; j++)
+        {
+            if (board[j][col] == num)
+                return false;
+        }
+        int boxRow = row - row % 3;
+        int boxCol = col - col % 3;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[i + boxRow][j + boxCol] == num)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+    bool Solve(vector<vector<char>> &board)
+    {
+        int row = 0, col = 0;
+        bool isEmpty = false;
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (board[i][j] == '.')
+                {
+                    row = i;
+                    col = j;
+                    isEmpty = true;
+                    break;
+                }
+            }
+        }
+        if (isEmpty == false)
+            return true;
+        for (char c = '1'; c <= '9'; c++)
+        {
+            if (isSafe(board, row, col, c))
+            {
+                board[row][col] = c;
+                if (Solve(board))
+                    return true;
+
+                board[row][col] = '.';
+            }
+        }
+
+        return false;
+    }
+    void solveSudoku(vector<vector<char>> &board)
+    {
+        Solve(board);
+    }
+};
