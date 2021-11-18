ff0ce5c87f530d51d73a3a8d6dfd9d6219e8da99
SahilRout <sahilrout07@gmail.com>
2021-11-18 14:17:06 +0530

    DAY-29

---
+++ DAY 29/Furthest Building You Can Reach.cpp
@@ -0,0 +1,23 @@
+//1642. Furthest Building You Can Reach
+class Solution
+{
+public:
+    int furthestBuilding(vector<int> &arr, int bricks, int ladders)
+    {
+        priority_queue<int> pq;
+        for (int i = 0; i < arr.size() - 1; i++)
+        {
+            int d = arr[i + 1] - arr[i];
+            if (d > 0)
+                pq.push(-d);
+            if (pq.size() > ladders)
+            {
+                bricks += pq.top();
+                pq.pop();
+            }
+            if (bricks < 0)
+                return i;
+        }
+        return arr.size() - 1;
+    }
+};
